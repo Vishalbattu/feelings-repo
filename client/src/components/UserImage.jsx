1c7e64d7858b9d81460d00d7ea25910f77213409
EC2 Default User <ec2-user@ip-172-31-29-255.eu-west-2.compute.internal>
2024-02-08 17:14:53 +0000

    update

--- client/src/components/UserImage.jsx
+++ client/src/components/UserImage.jsx
@@ -8,7 +8,7 @@ const UserImage = ({ image, size = "60px" }) => {
         width={size}
         height={size}
         alt="user"
-        src={`http://localhost:3001/assets/${image}`}
+        src={`http://18.130.81.34:3001/assets/${image}`}
       />
     </Box>
   );
